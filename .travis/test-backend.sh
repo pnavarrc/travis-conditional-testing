d9f6128ae7d52e5293f21c64f0552057c420ea35
Pablo Navarro <pnavarrc@gmail.com>
2017-06-16 09:53:25 -0700

    fixes testing scripts

--- .travis/test-backend.sh
+++ .travis/test-backend.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 if [ `git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep '^backend/'` ]; then
-  echo "This branch has changes on the `backend` directory, running backend tests."
+  echo "This branch has changes on the 'backend' directory, running backend tests."
   npm run test-backend
 else
-  echo "This branch doesn't have changes on the `backend` directory, skipping backend tests."
+  echo "This branch doesn't have changes on the 'backend' directory, skipping backend tests."
 fi
