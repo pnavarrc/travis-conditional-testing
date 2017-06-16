a8a7ed020766f7a1d1c2560f61c76416893d6eff
Pablo Navarro <pnavarrc@gmail.com>
2017-06-16 09:45:44 -0700

    Message for skipping tests

--- .travis/test-frontend.sh
+++ .travis/test-frontend.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 if [ `git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep '^frontend/'` ]; then
   npm run test-frontend
+else
+  echo "No frontend changes, skipping frontend tests."
 fi
