71c38a28db43dfbcbe3b14c3cddfdf38cfea615e
Pablo Navarro <pnavarrc@gmail.com>
2017-06-16 10:13:26 -0700

    Fix copy of test scripts

--- .travis/test-backend.sh
+++ .travis/test-backend.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 if [ `git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep '^backend/'` ]; then
-  echo "This branch has changes on the 'backend' directory, running backend tests."
+  echo "Detected changes on the 'backend' directory, running backend tests."
   npm run test-backend
 else
-  echo "This branch doesn't have changes on the 'backend' directory, skipping backend tests."
+  echo "No changes on the 'backend' directory, skipping backend tests."
 fi
--- .travis/test-frontend.sh
+++ .travis/test-frontend.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 if [ `git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep '^frontend/'` ]; then
-  echo "This branch has changes on the 'frontend' directory, running front-end tests."
+  echo "Detected changes on the 'frontend' directory, running front-end tests."
   npm run test-frontend
 else
-  echo "This branch doesn't have changes on the 'frontend' directory, skipping front-end tests."
+  echo "No changes on the 'frontend' directory, skipping front-end tests."
 fi
