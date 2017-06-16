03755a0986c792ffa56ae88b2dc1626f68b56c94
Pablo Navarro <pnavarrc@gmail.com>
2017-06-16 09:51:13 -0700

    Better copy for test scripts

--- .travis/test-backend.sh
+++ .travis/test-backend.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
-echo $(pwd)
 if [ `git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep '^backend/'` ]; then
+  echo "This branch has changes on the `backend` directory, running backend tests."
   npm run test-backend
+else
+  echo "This branch doesn't have changes on the `backend` directory, skipping backend tests."
 fi
--- .travis/test-frontend.sh
+++ .travis/test-frontend.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 if [ `git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep '^frontend/'` ]; then
+  echo "This branch has changes on the `frontend` directory, running front-end tests."
   npm run test-frontend
 else
-  echo "No frontend changes, skipping frontend tests."
+  echo "This branch doesn't have changes on the `frontend` directory, skipping front-end tests."
 fi
