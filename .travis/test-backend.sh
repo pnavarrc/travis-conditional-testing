7384a9fe2486085168ace52d6da9e81f8ae581f8
Pablo Navarro <pnavarrc@gmail.com>
2017-06-16 10:10:23 -0700

    Remove logging

--- .travis/test-backend.sh
+++ .travis/test-backend.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env sh
-echo "Commit range"
-echo $TRAVIS_COMMIT_RANGE
-
-echo "Files changed:"
-echo $(git diff --name-only "$TRAVIS_COMMIT_RANGE")
-
 if [ `git diff --name-only "$TRAVIS_COMMIT_RANGE" | grep '^backend/'` ]; then
   echo "This branch has changes on the 'backend' directory, running backend tests."
   npm run test-backend
